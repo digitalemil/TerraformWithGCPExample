f29ce003343bc0602f8e00e94332c05d2ae0ff21
digitalemil <digitalemil@googlemail.com>
2020-06-25 14:32:35 +0000

    mit kubectl

---
+++ create-config.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+export REGION=$(terraform output location)
+export CERTIFICATE_CA=$(terraform output ca_certificate)
+export NAME=$(terraform output cluster_name)
+export SERVER=$(terraform show | grep kubernetes_endpoint | awk '{ print $3 }' | sed 's/"//g')
+export PROJECT_ID=$(terraform output project_id)
+
+sed 's/%NAME%/'"$NAME"'/g' config.template >config
+sed -i 's/%REGION%/'"$REGION"'/g' config
+sed -i 's/%CERTIFICATE_CA%/'"$CERTIFICATE_CA"'/g' config
+sed -i 's/%SEVER%/'"$SERVER"'/g' config
+sed -i 's/%PROJECT_ID%/'"$PROJECT_ID"'/g' config
