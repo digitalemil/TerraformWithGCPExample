3087eb95336762e730b77df68f9348d8b2365783
digitalemil <digitalemil@googlemail.com>
2020-06-26 06:57:59 +0000

    create-config fix

--- create-config.sh
+++ create-config.sh
@@ -4,7 +4,11 @@ export CERTIFICATE_CA=$(./terraform output ca_certificate)
 export NAME=$(./terraform output cluster_name)
 export SERVER=$(./terraform show | grep kubernetes_endpoint | awk '{ print $3 }' | sed 's/"//g')
 export PROJECT_ID=$(./terraform output project_id)
-
+echo =======
+echo $SERVER
+echo $NAME
+echo $CERTIFICATE_CA
+echo =======
 sed 's/%NAME%/'"$NAME"'/g' config.template >config
 sed -i 's/%REGION%/'"$REGION"'/g' config
 sed -i 's/%CERTIFICATE_CA%/'"$CERTIFICATE_CA"'/g' config
